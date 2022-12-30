fdd451947dedd17ea892adbfd26ba16906bf8450
guiyun <ryougicloud@gmail.com>
2022-12-30 15:00:42 +0800

    修改Token验证

--- app-internal/src/main/java/plus/guiyun/app/framework/config/SecurityConfig.java
+++ app-internal/src/main/java/plus/guiyun/app/framework/config/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.filter.CorsFilter;
 import plus.guiyun.app.framework.security.filter.JwtAuthenticationTokenFilter;
 import plus.guiyun.app.framework.security.handle.AuthenticationEntryPointImpl;
@@ -21,10 +24,16 @@ public class SecurityConfig {
     private JwtAuthenticationTokenFilter authenticationTokenFilter;
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // CSRF禁用，因为不使用session
                 .csrf().disable()
+                .headers().frameOptions().disable().and()
                 // 基于token，所以不需要session
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 // 基于token，所以不需要session
