5971b6e5b226a6ed43a5932a07ea2ab53cd8ca28
孟源运 <ryougicloud@gmail.com>
2023-01-29 15:25:34 +0800

    fix:尝试修复bug index.vue:1 Failed to load module script: Expected a JavaScript module script but the server responded with a MIME type of "text/html"

--- app-ui/vite.config.js
+++ app-ui/vite.config.js
@@ -7,6 +7,7 @@ import {createHtmlPlugin} from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [
         vue(),
         createHtmlPlugin({
