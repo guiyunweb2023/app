015e755b6c7e925c94230c805e135195859b7925
孟源运 <ryougicloud@gmail.com>
2023-01-29 16:19:14 +0800

    去除顶栏右侧

--- app-ui/src/layout/header-aside/layout.vue
+++ app-ui/src/layout/header-aside/layout.vue
@@ -21,14 +21,14 @@
         <!-- 顶栏右侧 -->
         <div class="d2-header-right" flex-box="0">
           <!-- 如果你只想在开发环境显示这个按钮请添加 v-if="$env === 'development'" -->
-          <d2-header-search @click="handleSearchClick"/>
-          <d2-header-log/>
-          <d2-header-fullscreen/>
-          <d2-header-theme/>
-          <d2-header-size/>
-          <d2-header-locales/>
-          <d2-header-color/>
-          <d2-header-user/>
+          <d2-header-search v-if="$env === 'development'" @click="handleSearchClick"/>
+          <d2-header-log v-if="$env === 'development'" />
+          <d2-header-fullscreen v-if="$env === 'development'" />
+          <d2-header-theme v-if="$env === 'development'" />
+          <d2-header-size v-if="$env === 'development'" />
+          <d2-header-locales v-if="$env === 'development'" />
+          <d2-header-color v-if="$env === 'development'" />
+          <d2-header-user v-if="$env === 'development'" />
         </div>
       </div>
       <!-- 下面 主体 -->
