f989c8e705557ca39e019e8ef538ae9a892b886c
孟源运 <ryougicloud@gmail.com>
2023-02-14 16:06:50 +0800

    删除帮助

--- app-ui/src/components/RightContent/index.tsx
+++ app-ui/src/components/RightContent/index.tsx
@@ -41,14 +41,14 @@ const GlobalHeaderRight: React.FC = () => {
 
   return (
     <div className={className}>
-      <span
-        className={actionClassName}
-        onClick={() => {
-          window.open('https://pro.ant.design/docs/getting-started');
-        }}
-      >
-        <QuestionCircleOutlined />
-      </span>
+      {/*<span*/}
+      {/*  className={actionClassName}*/}
+      {/*  onClick={() => {*/}
+      {/*    window.open('https://pro.ant.design/docs/getting-started');*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <QuestionCircleOutlined />*/}
+      {/*</span>*/}
       <Avatar />
       <SelectLang className={actionClassName} />
     </div>
