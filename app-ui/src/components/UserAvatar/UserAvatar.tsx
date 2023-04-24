0b0734315f3076666f682fee7d6f3556e2a819cb
孟源运 <ryougicloud@gmail.com>
2023-04-24 18:01:11 +0800

    设置头像名称

--- app-ui/src/components/UserAvatar/UserAvatar.tsx
+++ app-ui/src/components/UserAvatar/UserAvatar.tsx
@@ -2,31 +2,33 @@ import React from "react";
 // @ts-ignore
 import {Avatar, Button, Dropdown, MenuProps} from "antd";
 import {history} from "@@/core/history";
+import Cookies from "js-cookie";
+import {useModel} from "@umijs/max";
 
-const items: MenuProps['items'] = [
-    {
-        key: 'logout',
-        label: '退出登录',
-        onClick: ()=>{
-            console.log("退出登录")
-            clearAllCookie()
-            history.push('/login');
-        }
-    },
-];
-
-function clearAllCookie() {
-    let keys = document.cookie.match(/[^ =;]+(?=\=)/g);
-    if(keys) {
-        for(let i = keys.length; i--;)
-            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
-    }
-}
 
 const UserAvatar: React.FC = () => {
+
+    const {initialState} = useModel('@@initialState');
+
+
+    const items: MenuProps['items'] = [
+        {
+            key: 'logout',
+            label: '退出登录',
+            onClick: () => {
+                console.log("退出登录")
+                Cookies.remove("token")
+                history.push('/login');
+            }
+        },
+    ];
+
+    const username: string = initialState.username
+
+    // @ts-ignore
     return (
         <Dropdown menu={{items}}>
-            <Avatar>孟源运</Avatar>
+            <Avatar>{username}</Avatar>
         </Dropdown>
     )
 }
