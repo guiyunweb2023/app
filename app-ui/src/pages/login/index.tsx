ccd5db06cd72afcd60d6dceb7e1b0db1e6214c08
孟源运 <ryougicloud@gmail.com>
2023-04-25 10:32:20 +0800

    修改登录页面

--- app-ui/src/pages/login/index.tsx
+++ app-ui/src/pages/login/index.tsx
@@ -1,19 +1,22 @@
-import styles from './index.less';
 // @ts-ignore
-import {Button, Form, Input, message} from "antd";
-import {Helmet} from "react-helmet";
+import {message} from "antd";
 import React from "react";
 import {login} from "@/api/auth";
 import Cookies from 'js-cookie'
 import {history} from 'umi';
 import {useModel} from "@umijs/max";
 import {flushSync} from "react-dom";
+// @ts-ignore
+import {LoginFormPage} from "@ant-design/pro-form";
+// @ts-ignore
+import {ProFormText} from "@ant-design/pro-components";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
 
 export default function () {
 
     const {setInitialState} = useModel('@@initialState');
 
-    const onFinish = (values: any) => {
+    const submitLogin = (values: any) => {
         console.log('Success:', values);
         // let data = login(values)
         login(values).then(res => {
@@ -36,27 +39,44 @@ export default function () {
 
 
     return (
-        <div className={styles.app}>
-            <Helmet>
-                <meta charSet="utf-8"/>
-                <title>登录</title>
-            </Helmet>
-            <div className={styles.login}>
-                <h1>登录</h1>
-                <Form autoComplete="off" onFinish={onFinish}>
-                    <Form.Item name="account" rules={[{required: true, message: '请输入账户名'}]}>
-                        <Input placeholder="用户名"/>
-                    </Form.Item>
-                    <Form.Item name="password" rules={[{required: true, message: '请输入密码'}]}>
-                        <Input.Password placeholder="密码"/>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" block>
-                            登录
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
+        <div style={{backgroundColor: '#1f5abc', height: '100vh'}}>
+            <LoginFormPage
+                logo='https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg'
+                title="登录"
+                onFinish={async (values) => {
+                    await submitLogin(values);
+                }}
+                backgroundImageUrl="https://file.iviewui.com/admin-cloud-dist/img/banner-3.8fb23f4b.png">
+                <div style={{ marginBlock: '48px 0' }}>
+                    <ProFormText
+                        name="account"
+                        fieldProps={{
+                            prefix: <UserOutlined className={'prefixIcon'}/>,
+                        }}
+                        placeholder={'请输入用户名'}
+                        rules={[
+                            {
+                                required: true,
+                                message: '用户名不能为空!',
+                            },
+                        ]}
+                    />
+
+                    <ProFormText.Password
+                        name="password"
+                        fieldProps={{
+                            prefix: <LockOutlined className={'prefixIcon'}/>,
+                        }}
+                        placeholder={'请输入密码'}
+                        rules={[
+                            {
+                                required: true,
+                                message: '密码不能为空！',
+                            },
+                        ]}
+                    />
+                </div>
+            </LoginFormPage>
         </div>
     )
 }
